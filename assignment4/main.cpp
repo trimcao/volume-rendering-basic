c6ac5a77ba24b4678c2e47b2923e6de8807a659f
Tri Cao <trimcao@Tris-MacBook-Pro.local>
2018-04-07 15:33:33 -0500

    Initial Commit

---
+++ assignment4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment4
+//
+//  Created by Tri M. Cao on 4/7/18.
+//  Copyright © 2018 Tri M. Cao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
